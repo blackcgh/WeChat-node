2e21f4f158e138be66215e798182f759e9c72cf3
black <1980271172@qq.com>
2020-03-28 19:24:03 +0800

    完成

--- routes/user.js
+++ routes/user.js
@@ -74,15 +74,10 @@ router.post('/login', async (req, res, next) => {
 // $route POST /api/user/register
 // @desc  注册
 router.post('/register', async (req, res, next) => {
-  const {
-    username,
-    password
-  } = req.body;
+  const { username, password } = req.body;
   const result = await register(username, password);
   if (result) {
-    res.json(new SuccessModel({
-      username: result.username
-    }, '注册成功！'))
+    res.json(new SuccessModel({ username: result.username }, '注册成功！'))
     return
   }
   res.json(new ErrorModel('注册失败！'))
@@ -104,10 +99,7 @@ router.get('/search', async (req, res, next) => {
 // $route POST /api/user/add
 // @desc  添加朋友
 router.post('/add', async (req, res, next) => {
-  const {
-    obj,
-    receiveOne
-  } = req.body;
+  const { obj, receiveOne } = req.body;
   await add(obj, receiveOne);
   res.json(new SuccessModel('已发送'))
 })
@@ -115,10 +107,7 @@ router.post('/add', async (req, res, next) => {
 // $route POST /api/user/agree
 // @desc  同意添加
 router.post('/agree', async (req, res, next) => {
-  const {
-    agreeObj,
-    requestObj
-  } = req.body;
+  const { agreeObj, requestObj } = req.body;
   await agree(agreeObj, requestObj);
   res.json(new SuccessModel('添加成功'))
 })
@@ -132,7 +121,6 @@ router.post('/upload', async (req, res, next) => {
   form.parse(req, (err, fields, files) => {
     const id = Object.keys(files)[0];
     const avatar = 'http://localhost/images/' + id;
-    // const path = './public/images/' + files[id].name;
     const path = './public/images/' + id;
     fs.rename(files[id].path, path, async function (err) {
       if (err) console.log(err);
